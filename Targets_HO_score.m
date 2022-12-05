c4727ed9afd0e8fff5542ac7b8a5ca8e4e75f8f2
Lavanya Raajaraam <33874803+Lav22@users.noreply.github.com>
2022-12-05 18:17:02 +0530

    Update Targets_HO_score.m

--- Targets_HO_score.m
+++ Targets_HO_score.m
@@ -1,5 +1,6 @@
 % to test and score higher-order interventions
 function [TargetsScoreTable] = Target_HO_score(model,multiFseof)
+
 %model: the GSMM with appropriate medium bounds applied
 %multiFseof: A matrix of reaction for product A,reaction for product B, amplification targets, deletion targets
 %TargetScoreTable: A table with all interventions and scores
@@ -13,11 +14,14 @@ AmpKO_3 = testresultsFVAMixedTargets(model,multiFseof{:,1},multiFseof{:,2},multi
 
 [~,maxFlux] = fluxVariability(model,100,'max',multiFseof{1,1});
 Targets = vertcat(Amp,KO,AmpKO_2,AmpKO_3);
+modelSol = optimizeCbModel(model);
+
 for i=1: size(Targets)
     Targets{i,6} = (Targets{i,2}-maxFlux(1))/(modelSol.f-Targets{i,4});
     Targets{i,7} = (Targets{i,3}-maxFlux(2))/(modelSol.f-Targets{i,4});
     Targets{i,8} = Targets{i,6}+Targets{i,7};
 end
+
 TargetsScoreTable = sortrows(Targets,8,{'descend'}); %sorted in descending order of Score A+B
 
 header = {'Intervention set','Mutant flux of prd A','Mutant flux of prd B','Mutant biomass flux','Type of intervention','Score A','Score B','Score A+B'};
