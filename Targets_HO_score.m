6bc5d02aa80b6a9da9aa788128a4a193b9046c1b
Lavanya Raajaraam <33874803+Lav22@users.noreply.github.com>
2022-12-08 19:33:39 +0530

    Update Targets_HO_score.m

--- Targets_HO_score.m
+++ Targets_HO_score.m
@@ -1,5 +1,5 @@
 % to test and score higher-order interventions
-function [TargetsScoreTable] = Targets_HO_score(model,multiFseof)
+function [TargetsScoreTable] = Targets_HO_score(model,minBM,multiFseof)
 
 %model: the GSMM with appropriate medium bounds applied
 %multiFseof: A matrix of reaction for product A,reaction for product B, amplification targets, deletion targets
@@ -9,8 +9,8 @@ function [TargetsScoreTable] = Targets_HO_score(model,multiFseof)
 [Amp, KO, AmpKO_2, AmpKO_3] = deal({});
 Amp = testresultsFVATargets(model,multiFseof{:,1},multiFseof{:,2},{'amp'}); %multiple amps (A)
 KO = testresultsFVATargets(model,multiFseof{:,1},multiFseof{:,3},{'ko'}); %multiple KOs (K)
-AmpKO_2 = testresultsFVAMixedTargets(model,multiFseof{:,1},multiFseof{:,2},multiFseof{:,3},2); %Amp+KO (AK)
-AmpKO_3 = testresultsFVAMixedTargets(model,multiFseof{:,1},multiFseof{:,2},multiFseof{:,3},3); %Amp+Amp+KO, Amp+KO+KO (AAK,AKK)
+AmpKO_2 = testresultsFVAMixedTargets(model,minBM,multiFseof{:,1},multiFseof{:,2},multiFseof{:,3},2); %Amp+KO (AK)
+AmpKO_3 = testresultsFVAMixedTargets(model,minBM,multiFseof{:,1},multiFseof{:,2},multiFseof{:,3},3); %Amp+Amp+KO, Amp+KO+KO (AAK,AKK)
 
 [~,maxFlux] = fluxVariability(model,100,'max',multiFseof{1,1});
 Targets = vertcat(Amp,KO,AmpKO_2,AmpKO_3);
