2b74cd1f5f9371cf8adb6078e322b42a99659097
Lavanya Raajaraam <33874803+Lav22@users.noreply.github.com>
2022-12-08 19:31:07 +0530

    Update Coproduction_masterScript.m

--- Coproduction_masterScript.m
+++ Coproduction_masterScript.m
@@ -4,29 +4,28 @@ solverOK = changeCobraSolver('ibm_cplex','all');
 
 %%choose organism and conditions
 
-% %ecoli as organism
+%%ecoli as organism
 %model = readCbModel('iML1515.xml'); %for GSMM
 model = readCbModel('e_coli_core.xml'); %core model
-
-%%Anaerobic condition
-% model = changeRxnBounds(model,{'EX_o2_e'},0,'b');
 
 %%yeast as organism
 % model = readCbModel('iMM904.xml');
 % %Anaerobic condition %anaerobic growth of yeast requires fatty acid supplementation in medium
 % model = changeRxnBounds(model,{'EX_ergst_e';'EX_zymst_e';'EX_hdcea_e';'EX_ocdca_e';'EX_ocdcea_e'; 'EX_ocdcya_e' },-1000,'l');
 % model = changeRxnBounds(model,{'EX_ergst_e';'EX_zymst_e';'EX_hdcea_e';'EX_ocdca_e';'EX_ocdcea_e'; 'EX_ocdcya_e' },1000,'u');
+
+%%Anaerobic condition
 % model = changeRxnBounds(model,{'EX_o2_e'},0,'b');
 
 %% to study co-production for all pairs of metabolites in a model
+minBM = 0.25; %minimum biomass of mutant - given in percentage of wild-type biomass
 modelSol = optimizeCbModel(model);
-[FseofAll,multiFseof,Amp,KO] = multiFSEOF(model);
+[FseofAll,multiFseof,Amp,KO] = multiFSEOF(model,minBM);
 multiFseofScoreTable = multiFseof_Score(model,multiFseof,Amp,KO);
 
 %% to study co-production for a single set of metabolites
 
 %TargetProducts = {'EX_ibutoh_e';'EX_succ_e'}; %example - for iMM904 model
-TargetProducts = {'EX_etoh_e';'EX_succ_e'}; %example - for e_coli_core model
-
+TargetProducts = {'EX_etoh_e';'EX_succ_e'}; %example - for e_coli_core model or iML1515 model
 [FseofAll,multiFseof]= FSEOFTargets(model,TargetProducts);
-TargetsScoreTable = Targets_HO_score(model,multiFseof);
+TargetsScoreTable = Targets_HO_score(model,minBM,multiFseof);
